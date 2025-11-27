99f35d7cd7f117d0610420a5e2cc02922b8c9e96
aishwarya.ramle <aishwarya.ramle@AISHWARYA-Linux.vdi.com>
2025-11-27 10:37:26 +0530

    Added variable a and b and printed its value

--- JavaDemo/src/day1/day1.java
+++ JavaDemo/src/day1/day1.java
@@ -4,7 +4,10 @@ public class day1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
 System.out.println("Hello World");
+System.out.println(a+b);
 	}
 
 }
